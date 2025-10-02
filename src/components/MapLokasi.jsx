eba71d3826688235652342239845c7b9c8551a6c
unknown <chuboot.id@gmail.com>
2025-10-02 15:30:54 +0800

    add okupasion dan maps

---
+++ src/components/MapLokasi.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Perbaiki icon default Leaflet agar muncul
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+  iconUrl,
+  shadowUrl,
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const MapLokasi = ({ data }) => {
+    const position = data.LocationMap;
+  const markerRef = useRef(null);
+
+  useEffect(() => {
+    if (markerRef.current) {
+      markerRef.current.openPopup();
+    }
+  }, []);
+
+  return (
+    <div className="w-full h-64 md:h-80 rounded-lg overflow-hidden shadow">
+      <MapContainer
+        center={position}
+        zoom={14}
+        className="w-full h-full"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position} ref={markerRef}>
+          {console.log(position)}
+          <Popup autoClose={false} closeOnClick={false}>
+            Lokasi KEK {data.Title}
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapLokasi;
--- src/pages/AreaDetail.jsx
+++ src/pages/AreaDetail.jsx
@@ -20,11 +20,12 @@ import {
   Briefcase,
   Map,
   Layers,
+  LandPlot,
   CheckSquare,
 } from "lucide-react";
 import DashKekKesehatan from "../components/DashKekKesehatan";
 import { PieChart, Pie, Cell } from "recharts";
-
+import MapLokasi from "../components/MapLokasi";
 const AreaDetail = () => {
   const { areaId } = useParams();
   const navigate = useNavigate();
@@ -37,7 +38,8 @@ const AreaDetail = () => {
 
   useEffect(() => {
     fetch(
-      "https://script.google.com/macros/s/AKfycbzuZ4GYed2KUNvp6tqVhijo0du79igUdULyiYZHK_2cy4zoWj9Yt4ReEnqK8sFcTAO9/exec"
+      // "https://script.google.com/macros/s/AKfycbzuZ4GYed2KUNvp6tqVhijo0du79igUdULyiYZHK_2cy4zoWj9Yt4ReEnqK8sFcTAO9/exec"
+      "https://script.google.com/macros/s/AKfycbw9ykhJi8PY2HuwqYs_vkAbuh_KWi_HQf9XuxuVuxyxb8QoPars5blIgTOUTLrshTiY8A/exec"
     )
       .then((res) => res.json())
       .then((data) => {
@@ -200,89 +202,158 @@ const AreaDetail = () => {
           </div>
           {/* Other Menu */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
-            <div className="bg-white p-6 rounded-xl shadow hover:shadow-lg cursor-pointer">
-              <div className="flex items-center space-x-2">
+            <div className="bg-white p-6 rounded-xl shadow hover:shadow-lg cursor-pointer ">
+              <div className="flex items-center space-x-2 ">
                 <div className="bg-orange-100 text-orange-500 p-2 rounded-full">
-                  <MessageSquareQuote />
+                  <LandPlot />
                 </div>
                 <span className="font-semibold">Okupansi Lahan</span>
               </div>
-              <div className="flex items-center justify-between w-full my-4">
-              <div className="flex flex-col items-center">
-                <PieChart width={100} height={100}>
-                  <Pie
-                    data={[
-                      { name: "Dikuasai", value: 100 },
-                      {
-                        name: "Sisa",
-                        value:
-                          100 -
-                          (area["Lahan Dikuasai"] / area["Total Lahan"])* 100,
-                      },
-                    ]}
-                    cx={40}
-                    cy={40}
-                    innerRadius={25}
-                    outerRadius={35}
-                    startAngle={90}
-                    endAngle={450}
-                    dataKey="value"
-                  >
-                    <Cell fill="#f8cf2c" />
-                  </Pie>
-                </PieChart>
+              <div className="flex flex-col items-center justify-between w-full my-4 border border-amber-200 p-2 rounded-2xl">
+                <div className="flex justify-between items-center ">
+                  <PieChart width={200} height={200}>
+                    <Pie
+                      data={[
+                        {
+                          name: "Lahan Dikuasai",
+                          value:
+                            (area["Lahan Dikuasai"] / area["Total Lahan"]) *
+                            100,
+                        }, // Menggunakan nilai persentase aktual
+                        {
+                          name: "Lahan Tidak Dikuasai",
+                          value:
+                            ((area["Total Lahan"] - area["Lahan Dikuasai"]) /
+                              area["Lahan Dikuasai"]) *
+                            100,
+                        }, // Menggunakan nilai persentase aktual
+                      ]}
+                      cx={70}
+                      cy={70}
+                      innerRadius={35}
+                      outerRadius={45}
+                      startAngle={90}
+                      endAngle={450}
+                      dataKey="value"
+                    >
+                      {/* Definisikan warna untuk setiap Cell */}
+                      <Cell key="cell-0" fill="#f8cf2c" />{" "}
+                      {/* Warna untuk Lahan Dikuasai */}
+                      <Cell key="cell-1" fill="#cccccc" />{" "}
+                      {/* Warna untuk Lahan Tidak Dikuasai */}
+                    </Pie>
+                  </PieChart>
+                  <div className="text-right ">
+                    <p className="text-xs text-gray-600">Total Luas Lahan</p>
+                    <p className="text-xl font-bold">
+                      {area["Total Lahan"]} Ha
+                    </p>
+                  <div className="mt-2 text-xs text-gray-600 ">
+                    <p>Luas lahan telah dikuasai</p>
+                   
+                      {(
+                        (area["Lahan Dikuasai"] / area["Total Lahan"]) *
+                        100
+                      ).toFixed(2)}{" "}
+                      % ({area["Lahan Dikuasai"]} Ha dari Luas sesuai PP)
+                  </div>
+                  </div>
+                </div>
+                <div className="flex justify-between items-center ">
+                  <PieChart width={200} height={200}>
+                    <Pie
+                      data={[
+                        {
+                          name: "Lahan Dimanfaatkan",
+                          value:
+                            (area["Lahan Dimanfaatkan"] /
+                              area["Lahan Dikuasai"]) *
+                            100,
+                        }, // Menggunakan nilai persentase aktual
+                        {
+                          name: "Lahan Tidak Dimanfaatkan",
+                          value:
+                            ((area["Lahan Dikuasai"] -
+                              area["Lahan Dimanfaatkan"]) /
+                              area["Lahan Dikuasai"]) *
+                            100,
+                        }, // Menggunakan nilai persentase aktual
+                      ]}
+                      cx={70}
+                      cy={70}
+                      innerRadius={35}
+                      outerRadius={45}
+                      startAngle={90}
+                      endAngle={450}
+                      dataKey="value"
+                    >
+                      {/* Definisikan warna untuk setiap Cell */}
+                      <Cell key="cell-0" fill="#f8cf2c" />{" "}
+                      {/* Warna untuk Lahan Dikuasai */}
+                      <Cell key="cell-1" fill="#cccccc" />{" "}
+                      {/* Warna untuk Lahan Tidak Dikuasai */}
+                    </Pie>
+                  </PieChart>
+                  <div className="text-right ">
+                    <p className="text-xs text-gray-600">Total Pemanfaatan Lahan</p>
+                    <p className="text-xl font-bold">
+                      {area["Lahan Dimanfaatkan"]} Ha
+                    </p>
+                  <div className="mt-2 text-xs text-gray-600">
+                    Luas lahan yang dimanfaatkan
+                    <br />
+                    {area["Lahan Dimanfaatkan"]} Ha (
+                    {(
+                      (area["Lahan Dimanfaatkan"] / area["Lahan Dikuasai"]) *
+                      100
+                    ).toFixed(2)}{" "}
+                    % dari Luas sesuai PP )
+                  </div>
+                  </div>
+                </div>
               </div>
-              <div className="text-right">
-                <p className="text-xs text-gray-600">Total Luas Lahan</p>
-                <p className="text-xl font-bold">{area["Total Lahan"]} Ha</p>
-              </div>
-            </div>
-            <div className="mt-1 text-sm">
-              <p>Lahan telah dikuasai</p>
-              <p className="font-bold">
-                {((area["Lahan Dikuasai"] / area["Total Lahan"]) * 100).toFixed(2)} %
-              </p>
-              <p className="font-bold">{area["Lahan Dikuasai"]} Ha</p>
-            </div>
-            <div className="flex flex-col items-center">
-                <PieChart width={100} height={100}>
-                  <Pie
-                    data={[
-                      { name: "Dikuasai", value: 100 },
-                      { name: "Sisa", value: 100 },
-                    ]}
-                    cx={40}
-                    cy={40}
-                    innerRadius={25}
-                    outerRadius={35}
-                    startAngle={90}
-                    endAngle={450}
-                    dataKey="value"
-                  >
-                    <Cell fill="#f8cf2c" />
-                  </Pie>
-                </PieChart>
-              </div>
-            <div className="mt-2 text-xs text-gray-600">
-              Luas lahan yang dimanfaatkan
-              <br />
-              {area["Lahan Dimanfaatkan"]} Ha (
-              { ( (area["Lahan Dimanfaatkan"] / area["Lahan Dikuasai"]) * 100 ).toFixed(2) } %
-              dari Luas sesuai PP)
-            </div>
             </div>
             <div className="bg-white p-6 rounded-xl shadow hover:shadow-lg cursor-pointer">
               <div className="flex items-center space-x-2">
                 <div className="bg-blue-100 text-blue-500 p-2 rounded-full">
-                  <Image />
+                  <MapPin />
                 </div>
-                <span className="font-semibold">Gallery</span>
+                <span className="font-semibold">Lokasi KEK</span>
               </div>
-              
+              <div className="my-8  overflow-hidden border-1 rounded-2xl border-orange-300">
+                {/* Minimap */}
+
+                <MapLokasi data={area} />
+                {/* <div className="w-full h-64 rounded-lg overflow-hidden shadow">
+                  <iframe
+                    title="Mini Map"
+                    width="100%"
+                    height="100%"
+                    frameBorder="0"
+                    style={{ border: 0 }}
+                    src="https://www.openstreetmap.org/export/embed.html?layer=mapnik&marker=-8.73185327657203,115.23144358972414&map=18/-8.73185327657203/115.23144358972414"
+                    allowFullScreen=""
+                    aria-hidden="false"
+                    tabIndex="0"
+                  ></iframe>
+
+                  <div className="text-xs mt-1 text-gray-500">
+                    <a
+                      href="https://www.openstreetmap.org/?mlat=-8.73185327657203&mlon=115.23144358972414#map=18/-8.73185/115.23144"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="underline"
+                    >
+                      Lihat peta lebih besar
+                    </a>
+                  </div>
+                </div> */}
+              </div>
             </div>
           </div>
           {/* Pie Chart Section */}
-          <div className="flex flex-col items-start my-4 p-4 bg-white rounded-xl w-64 shadow hover:shadow-lg cursor-pointer">
+
+          {/* <div className="flex flex-col items-start my-4 p-4 bg-white rounded-xl w-64 shadow hover:shadow-lg cursor-pointer">
             <div className="bg-orange-500 text-white text-sm font-semibold px-2 py-1 rounded mb-2 w-max">
               Okupansi Lahan
             </div>
@@ -330,7 +401,8 @@ const AreaDetail = () => {
               {(area["Lahan Dimanfaatkan"] / area["Lahan Dikuasai"]) * 100} %
               dari Luas sesuai PP)
             </div>
-          </div>
+          </div> */}
+
           {/* Khusus untuk sektor Kesehatan */}
           {areaId === "sanur" && <DashKekKesehatan />}
         </main>
